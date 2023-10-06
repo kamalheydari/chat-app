eb1e97311f87caa86cfbe6e3afe48d02340126cb
kamal heydari <kamal3heydari@gmail.com>
2023-10-06 17:28:58 +0330

    reamove unused codes

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,16 +1,7 @@
 export { default as Login } from './auth/Login'
-export { default as NewPassword } from './auth/NewPassword'
 export { default as Register } from './auth/Register'
-export { default as Verify } from './auth/Verify'
-export { default as ResetPassword } from './auth/ResetPassword'
 
-export { default as Call } from './dashboard/Call'
 export { default as Chats } from './dashboard/Chats'
-export { default as Conversation } from './dashboard/Conversation'
 export { default as GeneralApp } from './dashboard/GeneralApp'
-export { default as Group } from './dashboard/Group'
-export { default as Profile } from './dashboard/Profile'
-export { default as Settings } from './dashboard/Settings'
-export { default as Contact } from './dashboard/Contact'
 
 export { default as NotFound } from './404'
